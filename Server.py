b8fa73a195bbc7842a0c24b777663b9ebde29394
mahaotian's debian <maht2001@163.com>
2024-06-12 14:54:03 +0800

    add:thread lock

--- Server.py
+++ Server.py
@@ -14,6 +14,7 @@ class Server(Udp):
         self.clients = []
         self.cur_client = None
         self._mouse = MouseController()
+        self.lock = threading.Lock()
 
     def msg_receiver(self):
         while True:
@@ -21,13 +22,15 @@ class Server(Udp):
             msg = Message.from_bytes(data)
             if msg.msg_type == MsgType.DEVICE_JOIN and addr not in self.clients:
                 self.clients.append(addr)
-                self.cur_client = addr  # 临时测试
+                # self.cur_client = addr  # 临时测试
                 self.sendto(Message(MsgType.SUCCESS_JOIN,
                                     f'{socket.gethostbyname(socket.gethostname())},{self._port}').to_bytes(), addr)
                 print(f"client {addr} connected")
             elif msg.msg_type == MsgType.MOUSE_BACK:
+                self.lock.acquire()
                 self.cur_client = None
                 self._mouse.move_to((3840, msg.data[1]))
+                self.lock.release()
 
     def start_msg_listener(self):
         self.msg_listener = threading.Thread(target=self.msg_receiver)
@@ -45,7 +48,8 @@ class Server(Udp):
             msg = Message(MsgType.MOUSE_MOVE, f"{x - last_pos[0]},{y - last_pos[1]}")
             if self.cur_client:
                 self.sendto(msg.to_bytes(), self.cur_client)
-            if self._mouse.get_position()[0] <= 20 or self._mouse.get_position()[1] <= 20 or self._mouse.get_position()[0] >= 3838 or self._mouse.get_position()[1] >= 2158:
+            if self._mouse.get_position()[0] <= 100 or self._mouse.get_position()[1] <= 100 or \
+                    self._mouse.get_position()[0] >= 3740 or self._mouse.get_position()[1] >= 2060:
                 self._mouse.move_to((500, 500))
             self._mouse.update_last_position()
             if self.cur_client is None:
@@ -66,12 +70,16 @@ class Server(Udp):
                 for event in events:
                     if isinstance(event, pynput.mouse.Events.Move):
                         x, y = event.x, event.y
-                        if x > 3838:
+                        if x > 3838 and self.cur_client is None:
                             self._mouse.focus = False
+                            self.lock.acquire()
                             self.cur_client = self.clients[0]
                             self.sendto(Message(MsgType.MOUSE_MOVE_TO, f'{x - 3838},{y}').to_bytes(), self.cur_client)
                             self._mouse.move_to((500, 500))
+                            self.lock.release()
                             break
+
             if not self._mouse.focus:
                 mouse_listener = self.add_mouse_listener()
                 mouse_listener.join()
+                self._mouse.focus = True
