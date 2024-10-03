e7ff5538f1ca5bb418db9895621c27aab715a746
mahaotian's debian <maht2001@163.com>
2024-10-03 22:02:01 +0800

    Support Multi-level file share

--- MobiNetS项目代码/src/controller/file_controller.py
+++ MobiNetS项目代码/src/controller/file_controller.py
@@ -1,16 +1,20 @@
-import json
 import os.path
 import threading
 import time
 
-from src.communication.message import Message, MsgType, File_Message
+from src.communication.message import File_Message
 from src.communication.my_socket import TcpClient, TCP_PORT
 from src.utils.device_storage import DeviceStorage
 from src.utils.file import File
 from src.utils.net import get_local_ip
 
 
-
+def remove_all_files(dir_path):
+    for root, dirs, files in os.walk(dir_path, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
 
 
 def send_to_device(ip, msg):
@@ -25,8 +29,10 @@ class FileController:
     def __init__(self):
         if not os.path.exists(self.FILE_DIR):
             os.mkdir(self.FILE_DIR)
+        else:
+            remove_all_files(self.FILE_DIR)
         self.file_list = []
-        self.file_name_set = set()
+        self.file_set = set()
         self.host = get_local_ip()
         self.lock = threading.Lock()
 
@@ -45,15 +51,14 @@ class FileController_server(FileController):
     def file_listener(self):
         while True:
             self.lock.acquire()  # 获取锁
-            files = os.listdir(self.FILE_DIR)
-            for file_name in files:
-                if file_name not in self.file_name_set:
-                    print("new file: ", file_name)
-                    new_file = File(file_name, os.path.getsize(self.FILE_DIR + "/" + file_name), self.host)
-                    new_file_data = open("shared_files/" + file_name, "rb").read()
+            for root, dirs, files in os.walk(self.FILE_DIR, topdown=False):
+                for name in files:
+                    path = os.path.join(root, name)
+                    new_file = File(name, path, os.path.getsize(path), self.host)
+                    new_file_data = open(path, "rb").read()
                     self.send_to_all(File_Message(new_file, new_file_data))
                     self.file_list.append(new_file)
-                    self.file_name_set.add(file_name)
+                    self.file_set.add(path)
             time.sleep(2)
             self.lock.release()
 
@@ -61,12 +66,12 @@ class FileController_server(FileController):
         self.lock.acquire()
         self.save_file_to_dir(file_msg.file.name, file_msg.data)
         self.file_list.append(file_msg.file)
-        self.file_name_set.add(file_msg.file.name)
+        self.file_set.add(file_msg.file.path)
         self.send_to_all(file_msg)
         self.lock.release()
 
     def send_to_all(self, file_msg):
-        device_storage=DeviceStorage()
+        device_storage = DeviceStorage()
         for device in device_storage.get_all_devices():
             if device.ip != self.host:
                 send_to_device(device.ip, file_msg)
@@ -80,14 +85,14 @@ class FileController_client(FileController):
     def file_listener(self):
         while True:
             self.lock.acquire()  # 获取锁
-            files = os.listdir("shared_files")
-            for file_name in files:
-                if file_name not in self.file_name_set:
-                    new_file = File(file_name, os.path.getsize("shared_files/" + file_name), self.host)
-                    new_file_data = open("shared_files/" + file_name, "rb").read()
+            for root, dirs, files in os.walk(self.FILE_DIR, topdown=False):
+                for name in files:
+                    path = os.path.join(root, name)
+                    new_file = File(name, path, os.path.getsize(path), self.host)
+                    new_file_data = open(path, "rb").read()
                     self.send_to_master(File_Message(new_file, new_file_data))
                     self.file_list.append(new_file)
-                    self.file_name_set.add(file_name)
+                    self.file_set.add(path)
             time.sleep(2)
             self.lock.release()
 
@@ -98,8 +103,8 @@ class FileController_client(FileController):
 
     def save_file(self, file_msg: File_Message):
         self.lock.acquire()
-        if file_msg.file.name not in self.file_name_set:
+        if file_msg.file.name not in self.file_set:
             self.save_file_to_dir(file_msg.file.name, file_msg.file_data)
             self.file_list.append(file_msg.file)
-            self.file_name_set.add(file_msg.file.name)
+            self.file_set.add(file_msg.file.path)
         self.lock.release()
--- MobiNetS项目代码/src/utils/file.py
+++ MobiNetS项目代码/src/utils/file.py
@@ -3,12 +3,13 @@ import json
 
 class File:
 
-    def __init__(self, name, size, host):
+    def __init__(self, name, path, size, host):
         self.name = name
+        self.path = path
         self.size = size
         self.host = host
 
     @staticmethod
     def from_json(json_str):
         data = json.loads(json_str)
-        return File(data["name"], data["size"], data["host"])
+        return File(data["name"], data['path'], data["size"], data["host"])
